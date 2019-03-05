6d7c30491d33a138777372993d88e57eb63099da
Nazar Broslavskyi <bnmstdkpi@gmail.com>
2019-03-05 17:05:34 +0200

    chore(structure): add basic logic of program

--- src/index.js
+++ src/index.js
@@ -1,5 +1,19 @@
-// PLEASE DON'T change function name
 module.exports = function makeExchange(currency) {
-    // Your code goes here!
-    // Return an object containing the minimum number of coins needed to make change
+    let moneyExchange = {};
+    let remainder;
+    let currenctCoins;
+
+ // H, Q, D, N and P
+
+    if(!currency || currency < 0) {
+    	return {};
+    } else if(currency > 10000) {
+    	return {error: "You are rich, my friend! We don't have so much coins for exchange"}
+    } else {
+
+    }
+
+    return moneyExchange;
 }
+
+// console.log(makeExchange(99999));
